31c2ab9d085a1965c8d98a26c2ace86d43ebf240
bbangiee <yoojin1117@gmail.com>
2025-02-10 02:25:02 +0900

    :lipstick: Scaffold UI 추가

---
+++ app/src/main/java/com/example/handwritingmvc/view/ScaffoldView.kt
@@ -0,0 +1,47 @@
+package com.example.handwritingmvc.view
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.handwritingmvc.Greeting
+
+@Composable
+fun ScaffoldView() {
+    Scaffold(modifier = Modifier.fillMaxSize(), topBar = { ScaffoldTopAppBar() }) { innerPadding ->
+        Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldTopAppBar() {
+    CenterAlignedTopAppBar(
+        title = { Text("필기") },
+        actions = {
+            IconButton(
+                onClick = {}
+            ) {
+                Icon(Icons.Default.Edit,contentDescription = null)
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewFunction() {
+    ScaffoldView()
+}
