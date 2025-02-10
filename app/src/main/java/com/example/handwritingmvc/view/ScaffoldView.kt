0f949c1ba41b57fe5c5358b89e2595cb42175b73
bbangiee <yoojin1117@gmail.com>
2025-02-11 01:02:49 +0900

    :recycle: IconButton 코드 함수로 분리

--- app/src/main/java/com/example/handwritingmvc/view/ScaffoldView.kt
+++ app/src/main/java/com/example/handwritingmvc/view/ScaffoldView.kt
@@ -3,7 +3,7 @@ package com.example.handwritingmvc.view
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -12,6 +12,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.handwritingmvc.Greeting
 
@@ -31,13 +32,18 @@ fun ScaffoldTopAppBar() {
     CenterAlignedTopAppBar(
         title = { Text("필기") },
         actions = {
-            IconButton(
-                onClick = {}
-            ) {
-                Icon(Icons.Default.Edit,contentDescription = null)
-            }
+            TopBarIconButtons(Icons.Rounded.Add) { }
         }
     )
+}
+
+@Composable
+fun TopBarIconButtons(icon: ImageVector, onClick: () -> Unit) {
+    IconButton(
+        onClick = onClick
+    ) {
+        Icon(imageVector = icon,contentDescription = null)
+    }
 }
 
 @Preview(showBackground = true)
