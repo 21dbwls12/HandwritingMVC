ddd37a4e416816f80e2cdf87c3472265009c4787
bbangiee <yoojin1117@gmail.com>
2025-02-19 23:38:17 +0900

    :lipstick: 메인 화면 파일 생성

---
+++ app/src/main/java/com/example/handwritingmvc/view/NoteView.kt
@@ -0,0 +1,9 @@
+package com.example.handwritingmvc.view
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NoteView() {
+    Box {  }
+}
