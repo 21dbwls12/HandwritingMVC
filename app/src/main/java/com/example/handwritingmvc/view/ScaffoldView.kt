764dc3667e628f22318a51b1cc56a7e1e35c27bf
bbangiee <yoojin1117@gmail.com>
2025-02-19 21:49:44 +0900

    :lipstick: Scaffold 구현
    - FloatingButton 추가
    - IconButton 추가

--- app/src/main/java/com/example/handwritingmvc/view/ScaffoldView.kt
+++ app/src/main/java/com/example/handwritingmvc/view/ScaffoldView.kt
@@ -1,38 +1,45 @@
 package com.example.handwritingmvc.view
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Add
-import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.handwritingmvc.Greeting
 
 @Composable
-fun ScaffoldView() {
-    Scaffold(modifier = Modifier.fillMaxSize(), topBar = { ScaffoldTopAppBar() }) { innerPadding ->
-        Greeting(
-            name = "Android",
-            modifier = Modifier.padding(innerPadding)
-        )
+fun ScaffoldView(content: @Composable (PaddingValues) -> Unit) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { ScaffoldTopAppBar() },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(imageVector = Icons.Rounded.Edit, contentDescription = null)
+            }
+        }
+    ) { innerPadding ->
+        content(innerPadding)
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScaffoldTopAppBar() {
-    CenterAlignedTopAppBar(
-        title = { Text("필기") },
+    TopAppBar(
+        title = { Text("필기를 작성하세요") },
         actions = {
-            TopBarIconButtons(Icons.Rounded.Add) { }
+            TopBarIconButtons(Icons.Rounded.Check) { }
+            TopBarIconButtons(Icons.Rounded.Delete) { }
         }
     )
 }
@@ -42,12 +49,6 @@ fun TopBarIconButtons(icon: ImageVector, onClick: () -> Unit) {
     IconButton(
         onClick = onClick
     ) {
-        Icon(imageVector = icon,contentDescription = null)
+        Icon(imageVector = icon, contentDescription = null)
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewFunction() {
-    ScaffoldView()
 }
