26fbc75adc01636ff86fc02f85d929c8dc407dda
bbangiee <yoojin1117@gmail.com>
2025-02-21 01:09:29 +0900

    :sparkles: 이미지 불러오는 controller 함수들 추가
    - 받아온 uri, coil model로 변환
    - 이미지 불러오는 전체적인 controller

--- app/src/main/java/com/example/handwritingmvc/controller/LoadingController.kt
+++ app/src/main/java/com/example/handwritingmvc/controller/LoadingController.kt
@@ -6,13 +6,39 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import coil3.compose.AsyncImagePainter
+import coil3.compose.LocalPlatformContext
+import coil3.compose.rememberAsyncImagePainter
+import coil3.request.ImageRequest
 import com.example.handwritingmvc.model.LoadingModel
 
 class LoadingController {
+    // 사진선택도구로 이미지 불러오는 전체 controller
     @Composable
-    fun GetUri() {
-        var selectUri by remember { mutableStateOf<Uri?>(null) }
+    fun controlImage(): AsyncImagePainter {
+        var selectedUri by remember { mutableStateOf<Uri?>(null) }
+        var selectedImagePainter by remember { mutableStateOf<AsyncImagePainter?>(null) }
 
-        selectUri = LoadingModel().loadImage()
+        selectedUri = getUri()
+        selectedImagePainter = sendImage(selectedUri)
+        return selectedImagePainter!!
+    }
+
+    // model에서 uri 가져오는 함수
+    @Composable
+    private fun getUri(): Uri? {
+        return LoadingModel().loadImage()
+    }
+
+    // coil에서 uri를 painter로 변환하고 반환하는 함수
+    @Composable
+    private fun sendImage(uri: Uri?): AsyncImagePainter {
+        val painter = rememberAsyncImagePainter(
+            model = ImageRequest.Builder(LocalPlatformContext.current)
+                .data(uri)
+                .build(),
+        )
+
+        return painter
     }
 }
