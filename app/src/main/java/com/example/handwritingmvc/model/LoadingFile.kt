659c7a24f47934ba466d9660c7177e9d132e2606
bbangiee <yoojin1117@gmail.com>
2025-02-21 17:34:58 +0900

    :bug: Model에서 uri 데이터를 저장, 관리하도록 변경
    - 변경된 uri를 업데이트해주는 함수
    - 저장할 uri 데이터 선언

--- app/src/main/java/com/example/handwritingmvc/model/LoadingFile.kt
+++ app/src/main/java/com/example/handwritingmvc/model/LoadingFile.kt
@@ -1,27 +1,31 @@
 package com.example.handwritingmvc.model
 
 import android.net.Uri
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 
 class LoadingModel {
+    // 이미지 uri를 저장
+    var savedUri by mutableStateOf<Uri?> (null)
+        // 위에 선언된 uri값이 외부에서 접근할 수 있지만 수정은 불가능하게 해줌
+        private set
 
+    // 변경된 uri값을 저장하는 함수
+    fun updateUri(uri: Uri?) {
+        savedUri = uri
+    }
 
     // 갤러리에서 사진 불러오기
-    @Composable
-    fun loadImage(): Uri? {
-        var selectUri: Uri? = null
-        val pickMedia =
-            rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-                selectUri = uri
-            }
-
-        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
-        return selectUri
-    }
+//    @Composable
+//    fun loadImage(): Uri? {
+//        var selectUri: Uri? = null
+//        val pickMedia =
+//            rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+//                selectUri = uri
+//            }
+//
+//        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+//        return selectUri
+//    }
 }
