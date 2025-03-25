ab3117e9b30d23128f6521a40adc22b84535998f
bbangiee <yoojin1117@gmail.com>
2025-03-25 23:18:54 +0900

    :sparkles: 사진이 선택되면 화면에 사진 표시
    - ImageController에서 model에서 저장하고 있는 값을 일일이 업데이트하는데 이걸 더 자동화할 수 있지 않을까?
    - NoteView에서 controller의 값을 직접 연결하는 게 맞는 건가?

--- app/src/main/java/com/example/handwritingmvc/controller/ImageController.kt
+++ app/src/main/java/com/example/handwritingmvc/controller/ImageController.kt
@@ -15,7 +15,7 @@ class ImageController(private val imageModel: ImageModel) {
     // _: backing properties 명명 규칙
     // 값이 class 내부에서만 변경될 수 있고 외부에서는 읽기만 가능하도록 선언
     private var _selectedImagePainter = mutableStateOf(imageModel.savedUri)
-    val selectedImagePainter: Uri? by _selectedImagePainter
+    val selectedImagePainter by _selectedImagePainter
 
     // ScaffoldView에서 사진선택도구 실행하고 결과값을 Model에서 관리할 수 있도록 연결하는 함수
     // 지금은 직접 연결하는데 추후에는 버튼이 클릭되었을 때 Boolean값을 받아서 사진선택도구를 여기서 실행할 수 있도록 변경해야 함
@@ -34,10 +34,10 @@ class ImageController(private val imageModel: ImageModel) {
 
     // coil에서 uri를 painter로 변환하고 반환하는 함수
     @Composable
-    private fun sendImage(uri: Uri?): AsyncImagePainter {
+    fun sendImage(): AsyncImagePainter {
         val painter = rememberAsyncImagePainter(
             model = ImageRequest.Builder(LocalPlatformContext.current)
-                .data(uri)
+                .data(selectedImagePainter)
                 .build(),
         )
 
--- app/src/main/java/com/example/handwritingmvc/view/NoteView.kt
+++ app/src/main/java/com/example/handwritingmvc/view/NoteView.kt
@@ -1,15 +1,16 @@
 package com.example.handwritingmvc.view
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import coil3.compose.AsyncImage
 import com.example.handwritingmvc.controller.ImageController
 
 @Composable
 fun NoteView(imageController: ImageController) {
-    val painter = imageController.selectedImagePainter
-
     AsyncImage(
-        model = painter,
-        contentDescription = null
+        model = imageController.selectedImagePainter,
+        contentDescription = null,
+        modifier = Modifier.fillMaxSize()
     )
 }
