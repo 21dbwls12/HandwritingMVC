a571f872a6c270d483038a691ebe2d8a9724e2de
bbangiee <yoojin1117@gmail.com>
2025-03-25 22:09:04 +0900

    :bulb: ImageModel 주석 제거

--- app/src/main/java/com/example/handwritingmvc/model/ImageModel.kt
+++ app/src/main/java/com/example/handwritingmvc/model/ImageModel.kt
@@ -15,17 +15,4 @@ class ImageModel {
     fun updateUri(uri: Uri?) {
         savedUri = uri
     }
-
-    // 갤러리에서 사진 불러오기
-//    @Composable
-//    fun loadImage(): Uri? {
-//        var selectUri: Uri? = null
-//        val pickMedia =
-//            rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-//                selectUri = uri
-//            }
-//
-//        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
-//        return selectUri
-//    }
 }
