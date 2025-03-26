5307435152c77e04c3e10bf52556d43f979fb734
bbangiee <yoojin1117@gmail.com>
2025-03-26 15:33:04 +0900

    :bug: commit하기 전에 코드 정리하다가 수정한 코드 삭제된 듯하여서 다시 수정

--- app/src/main/java/com/example/handwritingmvc/controller/ImageController.kt
+++ app/src/main/java/com/example/handwritingmvc/controller/ImageController.kt
@@ -17,11 +17,17 @@ class ImageController(private val imageModel: ImageModel) {
     private var _selectedImagePainter = mutableStateOf(imageModel.savedUri)
     val selectedImagePainter by _selectedImagePainter
 
+    private var _isDeleteWritingClicked = mutableStateOf(false)
+    val isDeleteWritingClicked by _isDeleteWritingClicked
+
     // ScaffoldView에서 사진선택도구 실행하고 결과값을 Model에서 관리할 수 있도록 연결하는 함수
     // 지금은 직접 연결하는데 추후에는 버튼이 클릭되었을 때 Boolean값을 받아서 사진선택도구를 여기서 실행할 수 있도록 변경해야 함
     fun updateImagePickerResult(uri: Uri?) {
         imageModel.updateUri(uri)
+        _selectedImagePainter.value = uri
     }
+
+
 
     @Composable
     private fun createPainter(context: Context, uri: Uri): AsyncImagePainter {
