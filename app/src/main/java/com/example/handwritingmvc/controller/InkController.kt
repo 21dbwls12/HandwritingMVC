428999125e9d46852d4661ad810baa2cf67b979a
bbangiee <yoojin1117@gmail.com>
2025-07-12 00:59:36 +0900

    :bug: 화면을 다시 그리지 않으면 선이 보이지 않았던 버그 수정
    - inkController.kt의 드래그를 멈추었을 때 코드 수정
      - builder()로 객체를 새로 생성하는 것으로 수정함.
      - 그냥 상태만 변경하는 것은 화면에 바로 적용이 안되고 객체를 새로 생성하면 화면에 바로 적용이 되는 것 같음.
    - 버그 발견
      - 화면을 드래그하면 바로 선이 보이는 것이 아니고 화면을 다시 그려야 선이 보임.(이미지 불러오기)
      - 획을 곡선으로 그렸을 때 선이 아니라 잘린 원형 도형으로 그려지는 문제가 있음.

--- app/src/main/java/com/example/handwritingmvc/controller/InkController.kt
+++ app/src/main/java/com/example/handwritingmvc/controller/InkController.kt
@@ -1,6 +1,5 @@
 package com.example.handwritingmvc.controller
 
-import android.util.Log
 import android.view.MotionEvent
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,7 +29,17 @@ class InkController {
             }
             MotionEvent.ACTION_UP -> {
                 strokeBuilder.addPoint(Ink.Point.create(x, y, t))
-                inkBuilder.addStroke((strokeBuilder.build()))
+                // model 파일에서 관리하도록 수정 해야함.
+                val lastInkBuilder = inkBuilder
+                // 화면을 새로 그리지 않아도 그린 획이 화면에 보이게 하기 위해 builder()로 코드 변경
+                // 이전 코드
+                // inkBuilder.addStroke(strokeBuilder.build())
+                inkBuilder = Ink.builder().apply {
+                    // 원래 그려져있던 획
+                    lastInkBuilder.build().strokes.forEach{ addStroke(it) }
+                    // 새로 그린 획
+                    addStroke(strokeBuilder.build() )
+                }
             }
             else -> {
                 inkBuilder.build()
--- app/src/main/java/com/example/handwritingmvc/view/NoteView.kt
+++ app/src/main/java/com/example/handwritingmvc/view/NoteView.kt
@@ -1,49 +1,25 @@
 package com.example.handwritingmvc.view
 
-import android.util.Log
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.input.pointer.pointerInteropFilter
 import coil3.compose.AsyncImage
 import com.example.handwritingmvc.controller.ImageController
 import com.example.handwritingmvc.controller.InkController
-import com.google.mlkit.vision.digitalink.Ink
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun NoteView(imageController: ImageController) {
-    // point 위치 추적
-//    var point by remember { mutableStateOf(Offset.Zero) }
-//    // 새로 그려지는 획의 점을 저장하는 리스트
-//    val points = remember { mutableListOf<Offset>() }
-//    // 새로 그려지고 있는 중인 획
-//    var path by remember { mutableStateOf(Path()) }
-//    // 다 그려진 획들을 저장하는 리스트
-//    val paths = remember { mutableListOf<Path>() }
 
     val inkController = remember { InkController() }
-    val writing = inkController.inkBuilder
-//    val ink = remember { mutableStateOf(Ink.builder().build()) }
-//    val stroke1 = Ink.Stroke.builder()
-//        .addPoint(Ink.Point.create(50f, 50f))
-//        .addPoint(Ink.Point.create(100f, 100f))
-//        .addPoint(Ink.Point.create(150f, 50f))
-//        .build()
-//    val updateInk = Ink.builder().addStroke(stroke1).build()
-//    ink.value = updateInk
+    val line = inkController.inkBuilder
 
 
     Box(
@@ -105,7 +81,7 @@ fun NoteView(imageController: ImageController) {
                     true
                 }
         ) {
-            writing.build().strokes.forEach { stroke ->
+            line.build().strokes.forEach { stroke ->
                 val path = Path().apply {
                     stroke.points.forEachIndexed { index, point ->
                         if (index == 0) {
