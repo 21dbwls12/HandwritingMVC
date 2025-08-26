665ef77dc6ab58d9c255bbf931349f0c9d38233b
bbangiee <yoojin1117@gmail.com>
2025-08-26 18:37:22 +0900

    :recycle: view와 controller 구분
    - NoteView 파일에 작성했던 "pointerInput" 관련 로직을 LiveInkController 파일로 이동
    - 기능 문제없이 작동
    - LiveData, observeAsState 사용

--- app/src/main/java/com/example/handwritingmvc/controller/LiveInkController.kt
+++ app/src/main/java/com/example/handwritingmvc/controller/LiveInkController.kt
@@ -11,12 +11,18 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
 class LiveInkController {
+    // point 위치 추적을 위한 State
     var point by mutableStateOf(Offset.Zero)
+    // 새로 그려지는 path 표시하기 위한 points State
     val points = mutableListOf<Offset>()
-    var path = mutableStateOf(Path())
+    // 새로 그려지고 있는 중인 획 State
+    private var _refactorPath = MutableLiveData(Path())
+
     private val _paths = MutableLiveData<MutableList<Pair<Path, DrawStyle>>>()
     private val _pathStyle = MutableLiveData(Stroke(width = 0.5f))
 
+    val refactorPath: LiveData<Path>
+        get() = _refactorPath
     val paths: LiveData<MutableList<Pair<Path, DrawStyle>>>
         get() = _paths
     val pathStyle: LiveData<Stroke>
@@ -33,5 +39,30 @@ class LiveInkController {
         _paths.value = pathList ?: _paths.value
     }
 
-//    fun onDrag
+    fun onDragStart(offset: Offset) {
+        point = offset
+        points.add(point)
+    }
+
+    fun onDrag(dragAmount: Offset) {
+        point += dragAmount
+        points.add(point)
+        // onDrag가 호출 될 때마다 바로 그려지는 획 표시
+        _refactorPath.value = Path()
+        points.forEachIndexed{ index, point ->
+            if (index == 0) {
+                // 획의 시작 지점을 해당 좌표로 옮기기
+                _refactorPath.value!!.moveTo(point.x, point.y)
+            } else {
+                // 처음 클릭한 것이 아니라면 직선 추가
+                _refactorPath.value!!.lineTo(point.x, point.y)
+            }
+        }
+    }
+
+    fun onDragEnd() {
+        addPath(Pair(refactorPath.value!!, Stroke(1f)))
+        points.clear()
+        _refactorPath.value = Path()
+    }
 }   
--- app/src/main/java/com/example/handwritingmvc/view/NoteView.kt
+++ app/src/main/java/com/example/handwritingmvc/view/NoteView.kt
@@ -6,33 +6,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
 import coil3.compose.AsyncImage
 import com.example.handwritingmvc.controller.ImageController
-import com.example.handwritingmvc.controller.InkController
 import com.example.handwritingmvc.controller.LiveInkController
 
 @Composable
 fun NoteView(imageController: ImageController) {
 
-    val inkController = remember { InkController() }
-    val line = inkController.inkBuilder
     val liveInkController = remember { LiveInkController() }
 
-    // point 위치 추적을 위한 State
-    var point by remember { mutableStateOf(Offset.Zero) }
-    // 새로 그려지는 path 표시하기 위한 points State
-    val points = remember { mutableListOf<Offset>() }
-    // 새로 그려지고 있는 중인 획 State
-    var path by remember { mutableStateOf(Path()) }
+    // 드래그하고 있는 상태를 실시간으로 추적하기 위한 변수
+    val refactorPath by liveInkController.refactorPath.observeAsState()
 
     val paths by liveInkController.paths.observeAsState()
 
@@ -48,44 +37,32 @@ fun NoteView(imageController: ImageController) {
             .pointerInput(Unit) {
                 detectDragGestures(
                     onDragStart = { offset ->
-                        point = offset
-                        points.add(point)
+                        liveInkController.onDragStart(offset)
                     },
                     onDrag = { _, dragAmount ->
-                        point += dragAmount
-                        points.add(point)
-                        // onDrag가 호출 될 때마다 바로 그려지는 획 표시
-                        path = Path()
-                        points.forEachIndexed { index, point ->
-                            if (index == 0) {
-                                // 획의 시작 지점을 해당 좌표로 옮기기
-                                path.moveTo(point.x, point.y)
-                            } else {
-                                // 처음 클릭한 것이 아니라면 직선 추가
-                                path.lineTo(point.x, point.y)
-                            }
-                        }
+                        liveInkController.onDrag(dragAmount)
                     },
                     onDragEnd = {
-//                        InkController()
-                        liveInkController.addPath(Pair(path, Stroke(1f)))
-                        points.clear()
-                        path = Path()
-
+                        liveInkController.onDragEnd()
                     }
                 )
             }
     ) {
+        // 그린 모든 선을 화면에 보여주기 위해 사용
         paths?.forEach {
-            drawPath(path = it.first, Color.White, style = it.second)
+            drawPath(path = it.first, Color.Black, style = it.second)
         }
 
-        drawPath(path, Color.White, style = Stroke(1f))
+        // 선이 드래그하는 동안 실시간으로 보여주기 위해서 사용
+        drawPath(refactorPath!!, Color.Black, style = Stroke(1f))
     }
 }
 
 // 실시간으로 그려지는 선이 보이지 않고 선을 다 그린 후에 나타남.
 // 처음에 작성한 코드
+//val inkController = remember { InkController() }
+//val line = inkController.inkBuilder
+
 //@OptIn(ExperimentalComposeUiApi::class)
 //Canvas(
 //    modifier = Modifier
@@ -111,4 +88,58 @@ fun NoteView(imageController: ImageController) {
 //        // 이후에 색 지정하는 기능을 추가하는 것을 목표로 설정.
 //        drawPath(path, Color.White)
 //    }
+//}
+
+// 실시간으로 그리는 선을 화면에 표시
+//val liveInkController = remember { LiveInkController() }
+
+//// point 위치 추적을 위한 State
+//var point by remember { mutableStateOf(Offset.Zero) }
+//// 새로 그려지는 path 표시하기 위한 points State
+//val points = remember { mutableListOf<Offset>() }
+//// 새로 그려지고 있는 중인 획 State
+//var path by remember { mutableStateOf(Path()) }
+
+//val paths by liveInkController.paths.observeAsState()
+
+//Canvas(
+//modifier = Modifier
+//.fillMaxSize()
+//.pointerInput(Unit) {
+//    detectDragGestures(
+//        onDragStart = { offset ->
+//            point = offset
+//            points.add(point)
+//        },
+//        onDrag = { _, dragAmount ->
+//            point += dragAmount
+//            points.add(point)
+//            // onDrag가 호출 될 때마다 바로 그려지는 획 표시
+//            path = Path()
+//            points.forEachIndexed { index, point ->
+//                if (index == 0) {
+//                    // 획의 시작 지점을 해당 좌표로 옮기기
+//                    path.moveTo(point.x, point.y)
+//                } else {
+//                    // 처음 클릭한 것이 아니라면 직선 추가
+//                    path.lineTo(point.x, point.y)
+//                }
+//            }
+//        },
+//        onDragEnd = {
+//            liveInkController.addPath(Pair(path, Stroke(1f)))
+//            points.clear()
+//            path = Path()
+//
+//        }
+//    )
+//}
+//) {
+//    // 그린 모든 선을 화면에 보여주기 위해 사용
+//    paths?.forEach {
+//        drawPath(path = it.first, Color.Black, style = it.second)
+//    }
+//
+//    // 선이 드래그하는 동안 실시간으로 보여주기 위해서 사용
+//    drawPath(path, Color.White, style = Stroke(1f))
 //}
