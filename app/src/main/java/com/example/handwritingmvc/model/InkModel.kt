55cf255354a5bceb29fa5242ad5808cbcd305e7f
bbangiee <yoojin1117@gmail.com>
2025-09-01 20:04:38 +0900

    :art: 그린 획에 관한 model 파일에 기본 함수 추가
    - 저장된 path를 업데이트하는 함수
    - 저장된 path를 제거하는 함수

--- app/src/main/java/com/example/handwritingmvc/model/InkModel.kt
+++ app/src/main/java/com/example/handwritingmvc/model/InkModel.kt
@@ -8,4 +8,14 @@ import androidx.compose.ui.graphics.Path
 class InkModel {
     var savedPath by mutableStateOf<Path?>(null)
         private set
+
+    // 사용자가 그린 획을 저장하는 함수
+    fun updatePath(path: Path) {
+        savedPath = path
+    }
+
+    // 저장된 획을 모두 지우는 함수
+    fun deletePath() {
+        savedPath = null
+    }
 }
